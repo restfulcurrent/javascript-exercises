7f5c18f0615043ad1e815a3a2addbeec5b264b79
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-06 17:49:29 -0500

    Solve  exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, numRepetitions) {
+    if (numRepetitions < 0) {
+        return "ERROR";
+    }
 
+    let repeatedStr = "";
+    for (let i = 0; i < numRepetitions; i++) {
+        repeatedStr += str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
