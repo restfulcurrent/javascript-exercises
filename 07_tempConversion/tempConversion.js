80780e40865be651839d05238e5b1fbb3d5c7cb0
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-08 13:29:25 -0500

    Add solution to exercise 7 passing all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempCelcius) {
+  const CONVERSION_OFFSET = 32;
+  const CONVERSION_FACTOR = 5 / 9;
+
+  const tempFarenheight = (tempCelcius - CONVERSION_OFFSET) * CONVERSION_FACTOR;
+  
+  return Math.round(tempFarenheight * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempFarenheight) {
+  const CONVERSION_OFFSET = 32;
+  const CONVERSION_FACTOR = 9 / 5;
+
+  const tempCelcius = (tempFarenheight  * CONVERSION_FACTOR + CONVERSION_OFFSET);
+
+  return Math.round(tempCelcius * 10) / 10;
 };
 
 // Do not edit below this line
