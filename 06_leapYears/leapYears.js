5f88be3eff468af83a4046f784f4ea370b6256ab
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-08 12:54:17 -0500

    Add solution to exercise 6 passing all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isYearDivisibleByOneHundred = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+    
+   return (isYearDivisibleByFour &&
+     (!isYearDivisibleByOneHundred || isYearDivisibleByFourHundred));
 };
 
 // Do not edit below this line
