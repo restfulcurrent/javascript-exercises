6fc557ef763347ab4aca6c9f20fe5972d81bb392
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-15 17:03:59 -0500

    Add solution that passes all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    if (people.length === 0) return {};
 
+    return people.reduce((oldest, current) => {
+        oldestAge = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+        currentAge = getAge(current.yearOfDeath, current.yearOfBirth); 
+        
+        return (oldestAge > currentAge) ? oldest : current;
+    });
+
+
+    function getAge(yearOfDeath, yearOfBirth) {
+        return (yearOfDeath ?? new Date().getFullYear()) - yearOfBirth;    
+    }
 };
 
 // Do not edit below this line
