d073dca7593ec4e84633022d595b1f60daceb8c2
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-15 16:02:57 -0500

    Add solution that passes all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n === "string") {
+        n = parseInt(n);
+    }
 
+    if (n < 0) return "OOPS";
+    
+    // fib(0) = 0, fib(1) = 1
+    const sequence = [0, 1];
+
+    if (n < 2) return sequence[n];    
+
+    // if n > 1 fib(n) = fib(n-1) + fib(n-2)
+    for (let i = 2; i <= n; i++) {
+        sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+
+    return sequence.at(-1);
 };
 
 // Do not edit below this line
