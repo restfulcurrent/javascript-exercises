94a1229177a006a9f2708ced430a72a26fc9d66c
restedcurrent <189034752+restfulcurrent@users.noreply.github.com>
2024-12-15 12:21:59 -0500

    Add solution passing all test cases

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const cleanString = str
+        .toLowerCase()
+        .split("") 
+        .filter(char => 
+            (char != char.toUpperCase()) || 
+            (isFinite(char) && char != " " && char != "")
+        ) // include only if char is a letter or number
+        .join(""); 
 
+    const reversedString = cleanString.split("").reverse().join("");
+
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
